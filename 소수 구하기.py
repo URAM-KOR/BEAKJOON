52a520d8bcaf975b66a442b530e2429b223d64fc
URAM-KOR <76554431+URAM-KOR@users.noreply.github.com>
2020-12-24 23:30:42 +0900

    Update 소수 구하기.py
    시간초과로 인한 알고리즘변경

--- 소수 구하기.py
+++ 소수 구하기.py
@@ -1,14 +1,16 @@
 #https://www.acmicpc.net/problem/1929
 
 def sosu(A):
+
+    if 2 in A:
+        print(2)
     for i in A:
-        for n in range(2,i+1):
-            if i%n == 0:
-                if n==i:
-                    print(i)
-                else:break
-            elif i//n != 1:
-                continue
+        a=i+1-i//2
+        for n in range(2,a):
+            if i%n==0:break
+            elif i//n<2:
+                print(i)
+
 A=[]
 x,y=map(int,input().split())
 for i in range(x,y+1):
